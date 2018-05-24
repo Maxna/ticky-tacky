ce978a96fde2b269a5ebacaf884c8e03603a9298
Matt Dowdney and Matt Nardoni <matt@>
2018-05-24 14:14:34 -0700

    add ascii grid

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class App {
@@ -13,11 +14,32 @@ public class App {
         while (programRunning) {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Welcome to Tic Tac Toe!");
-            System.out.println("diagram");
             System.out.println("Enter a number corresponding to a square to select.");
+
+            String [] firstBoardLine= {"1","|", "2","|","3"};
+            String [] secondBoardLine= {"4","|", "5","|","6"};
+            String [] thirdBoardLine= {"7","|", "8","|","9"};
+
             TicTacToe ticTacToe = new TicTacToe();
             try {
                 while(!ticTacToe.winCheck()) {
+
+                    for (String element: firstBoardLine)
+                    {
+                        System.out.print(element);
+                    }
+                    System.out.println("\n-----");
+                    for (String element: secondBoardLine)
+                    {
+                        System.out.print(element);
+                    }
+                    System.out.println("\n-----");
+                    for (String element: thirdBoardLine)
+                    {
+                        System.out.print(element);
+                    }
+                    System.out.println("\n");
+
 
                     if (ticTacToe.getCurrentUserLetter().equals("x")) {
                         System.out.println("Your move player 1(X):");
@@ -25,10 +47,34 @@ public class App {
                         System.out.println("Your move player 2(O):");
                     }
                     String userEntry = bufferedReader.readLine();
-                    ticTacToe.addLetterToBoard(Integer.parseInt(userEntry));
-                    ticTacToe.turnIncrement();
-                    ticTacToe.letterUpdate();
-                    ticTacToe.winCheck();
+
+                    if(!ticTacToe.addLetterToBoard(Integer.parseInt(userEntry))){
+                        System.out.println("That has already been chosen");
+                    } else {
+                        ticTacToe.addLetterToBoard(Integer.parseInt(userEntry));
+                        if(userEntry.equals("1")){
+                            firstBoardLine[0]= ticTacToe.getCurrentUserLetter();
+                        } else  if (userEntry.equals("2")){
+                            firstBoardLine[2]= ticTacToe.getCurrentUserLetter();
+                        } else  if (userEntry.equals("3")){
+                            firstBoardLine[4]= ticTacToe.getCurrentUserLetter();
+                        } else  if (userEntry.equals("4")){
+                            secondBoardLine[0]= ticTacToe.getCurrentUserLetter();
+                        } else  if (userEntry.equals("5")){
+                            secondBoardLine[2]= ticTacToe.getCurrentUserLetter();
+                        } else  if (userEntry.equals("6")){
+                            secondBoardLine[4]= ticTacToe.getCurrentUserLetter();
+                        } else  if (userEntry.equals("7")){
+                            thirdBoardLine[0]= ticTacToe.getCurrentUserLetter();
+                        } else  if (userEntry.equals("8")){
+                            thirdBoardLine[2]= ticTacToe.getCurrentUserLetter();
+                        } else  if (userEntry.equals("9")) {
+                            thirdBoardLine[4] = ticTacToe.getCurrentUserLetter();
+                        } else System.out.println("error");
+                        ticTacToe.turnIncrement();
+                        ticTacToe.letterUpdate();
+                        ticTacToe.winCheck();
+                    }
                     if (ticTacToe.winCheck()) {
                         System.out.println("You won brah.");
                     }
