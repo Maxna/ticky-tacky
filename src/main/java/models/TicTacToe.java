d56320c4b9a029782edf74d77f989d36f0791294
Matt Dowdney and Matt Nardoni <matt@>
2018-05-24 09:52:09 -0700

    add test for incrementing turncounter

--- src/main/java/models/TicTacToe.java
+++ src/main/java/models/TicTacToe.java
@@ -1,11 +1,45 @@
 package models;
 
 public class TicTacToe {
+
+
     private String[] gameBoard = {"1","2", "3", "4", "5", "6", "7", "8", "9"};
     private int turnCounter = 2;
+
+
+
+    private String currentUserLetter = "x";
 
     public TicTacToe(){
         this.gameBoard = gameBoard;
         this.turnCounter= turnCounter;
+        this.currentUserLetter = currentUserLetter;
     }
+
+
+    public String[] getGameBoard() {
+        return gameBoard;
+    }
+
+    public int getTurnCounter() {
+        return turnCounter;
+    }
+
+    public String getCurrentUserLetter() {
+        return currentUserLetter;
+    }
+
+    public boolean addLetterToBoard(int squareChoice) {
+        if (!gameBoard[squareChoice - 1].equals("x") || !gameBoard[squareChoice - 1].equals("o")) {
+            gameBoard[squareChoice - 1] = currentUserLetter;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void turnIncrement(){
+        this.turnCounter += 1;
+    }
+
+
 }
--- src/test/java/models/TicTacToeTest.java
+++ src/test/java/models/TicTacToeTest.java
@@ -6,9 +6,31 @@ import static org.junit.Assert.*;
 
 public class TicTacToeTest {
     @Test
-    public void newRectangle_instantiatesCorrectly() throws Exception {
+    public void newTicTacToe_instantiatesCorrectly() throws Exception {
         TicTacToe testTicTacToe = new TicTacToe();
         assertEquals(true, testTicTacToe instanceof TicTacToe);
+    }
+
+    @Test
+    public void addLetterToBoard_addsXToBoardArray_x() throws Exception {
+        TicTacToe testTicTacToe = new TicTacToe();
+        testTicTacToe.addLetterToBoard(1);
+        assertEquals("x", testTicTacToe.getGameBoard()[0]);
+    }
+    @Test
+    public void turnUpdate_changesCurrentTurnCounter_3() throws Exception {
+        TicTacToe testTicTacToe = new TicTacToe();
+        testTicTacToe.addLetterToBoard(1);
+        testTicTacToe.turnIncrement();
+        assertEquals(3, testTicTacToe.getTurnCounter());
+    }
+    @Test
+    public void letterUpdate_changesCurrentUserLetterBasedOnTurn_3() throws Exception {
+        TicTacToe testTicTacToe = new TicTacToe();
+        testTicTacToe.addLetterToBoard(1);
+        testTicTacToe.turnIncrement();
+        testTicTacToe.tur
+        assertEquals("o", testTicTacToe.getTurnCounter());
     }
 
 }
